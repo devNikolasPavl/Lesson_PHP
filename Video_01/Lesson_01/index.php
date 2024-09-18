c89396b765ebf70fdebcb09a2c470938370d3bd0
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-18 14:54:41 +0300

    Первый файл на php

---
+++ Video_01/Lesson_01/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+echo 'Привет мир!';
+echo '<br />';
+echo 'Hello, World!';
+echo '<br />';
+
+echo PHP_INT_MAX;
+echo '<br />';
+
+$x = 11;
+echo $x;
+echo '<br />';
+
+$y = 'String';
+echo $y;
+echo '<br />';
+
+$q = true;
+$w = false;
+echo "q = $q";
+echo '<br />';
+echo "w = $w";
+echo '<br />';
+
+
+
+?>
