6a02af9c35ec31edd17c334c54a6b23f622037f5
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-27 10:19:32 +0300

    Video_01, Ch_01, Lesson_03

---
+++ Video_01/Ch_01/Lesson_03/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$x = 5;
+$y = 3;
+
+$z = $x + $y;
+$z = $x - $y;
+
+echo "сумма 5 + 3 = $z";
+echo "<br>";
+echo "сумма 5 - 3 = $z";
+echo "<br>";
+
+$z = $x * $y;
+echo '5 * 3 = '.$z;
+echo "<br>";
+
+$z = $x / $y;
+echo '5 / 3 = '.$z;
+echo "<br>";
+
+$z = $x % $y;
+echo '5 % 3 = '.$z;
+echo "<br>";
+
+?>
