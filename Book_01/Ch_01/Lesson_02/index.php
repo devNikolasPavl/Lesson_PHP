1c406e9f92b8169c6e46dede5a0f08142e4f6386
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-09-30 09:49:46 +0300

    Book_01, Ch_01, Lesson_02

---
+++ Book_01/Ch_01/Lesson_02/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>echo Command</title>
+</head>
+<body>
+
+<h1>The Candy Store</h1>
+<h2><?php echo 'Ivy\'s'; ?></h2>
+
+<?php echo '<p class="offer">Offer: 20% off</p>' ?>
+
+</body>
+</html>
