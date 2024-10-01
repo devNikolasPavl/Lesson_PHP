ba3c27592774da0a653709aa8f6a3bcbad028465
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2024-10-01 11:59:27 +0300

    Book_01, Ch_03, Lesson_01

---
+++ Book_01/Ch_03/Lesson_01/candy.php
@@ -0,0 +1,12 @@
+
+<?php
+include 'header.php';
+?>
+
+<h1>The Candy Store</h1>
+<h2>Welcome</h2>
+<p>A wide selection of delicious candy handmade in our kitchen...</p>
+
+<?php
+include 'footer.php';
+?>
