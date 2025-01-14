de45d99f9de7978ea35d919212499ac6a6ad8b24
devNikolasPavl <112695803+devNikolasPavl@users.noreply.github.com>
2025-01-14 15:57:25 +0300

    Level_02, Lesson_05

---
+++ Metanit/Level_02/Lesson_04/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$a = 4;
+
+switch($a) 
+{
+    case 1:
+        echo "Отлично";
+        break;
+    case 2:
+        echo "Хорошо";
+        break;
+    case 3:
+        echo "Удовлетворительно";
+        break;
+    case 4:
+        echo "Плохо";
+        break;
+}
+
+?>
